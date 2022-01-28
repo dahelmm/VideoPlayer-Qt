a7eb1c15f23e5e8c6cd2c3c9fee35459fdc20729
dahelmm <dahelmm@mail.ru>
2022-01-28 22:18:55 +0300

    .

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFile>
+#include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    player = new QMediaPlayer(this);
+    videoWidget = new QVideoWidget;
+    player->setVideoOutput(videoWidget);
+    videoWidget->setGeometry(50,50,800,600);
+    videoWidget->show();
+
+    connect(player,SIGNAL(positionChanged(qint64)),this,SLOT(setPosition(qint64)));
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +22,32 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_action_triggered()
+{
+    player->setMedia(QUrl(QFileDialog::getOpenFileName(this,"Выберите видео","","(*.mp4)")));
+    player->setVolume(50);
+    ui->prB_volume->setValue(player->volume());
+    qDebug()<<player->duration();
+
+}
+
+void MainWindow::on_pB_play_clicked()
+{
+    player->play();
+    ui->prB_time->setMaximum(player->duration()/1000);
+}
+
+void MainWindow::on_pB_pause_clicked()
+{
+    player->pause();
+
+}
+
+void MainWindow::setPosition(qint64 position)
+{
+    int pos = position/1000;
+
+    ui->prB_time->setValue(pos);
+
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMediaPlayer>
+#include <QMediaContent>
+#include <QVideoWidget>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +18,18 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_action_triggered();
+
+    void on_pB_play_clicked();
+
+    void on_pB_pause_clicked();
+    void setPosition(qint64);
+
 private:
     Ui::MainWindow *ui;
+    QMediaPlayer *player;
+    QMediaContent *content;
+    QVideoWidget *videoWidget;
 };
 #endif // MAINWINDOW_H
